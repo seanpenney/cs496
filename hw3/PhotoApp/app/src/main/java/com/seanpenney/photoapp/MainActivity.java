9f44873c2c779119f421a0e82f49ad485251640e
seanpenney <seanjpenney@gmail.com>
2015-05-11 23:51:48 -0700

    started navigation drawer

--- hw3/PhotoApp/app/src/main/java/com/seanpenney/photoapp/MainActivity.java
+++ hw3/PhotoApp/app/src/main/java/com/seanpenney/photoapp/MainActivity.java
@@ -2,13 +2,19 @@ package com.seanpenney.photoapp;
 
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.media.MediaActionSound;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -20,12 +26,36 @@ public class MainActivity extends ActionBarActivity {
     private Camera camera;
     private int cameraId = 0;
     SurfaceView surfaceViewPreview;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerList = (ListView) findViewById(R.id.navList);
         surfaceViewPreview = (SurfaceView) findViewById(R.id.surfaceView1);
+
+        addDrawerItems();
+
+        /* Add action bar toggle switch */
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        setupDrawer();
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG).show();
@@ -61,6 +91,10 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if(mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
@@ -119,5 +153,31 @@ public class MainActivity extends ActionBarActivity {
             camera = null;
         }
         super.onPause();
+    }
+
+    private void addDrawerItems() {
+        String[] itemsArray = {"View Photos", "About"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemsArray);
+        mDrawerList.setAdapter(mAdapter);
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 }
